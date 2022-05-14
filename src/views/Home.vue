db64bdc923b3ff709cb1ea45a38db6337d491ea1
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-05-15 00:37:07 +0430

    Home Page Section 1 (nc)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
   <div id="home" data-safe>
     <section data-s-1 dir="rtl">
       <div class="row">
+        <div></div>
         <div class="col-sm-6 d-flex flex-column justify-content-start">
           <h1>آژانس خلاقیت ویدا</h1>
           <p>تسا مزال هک نانچنآرطس و نوتس رد هلجم وچ تعنص زا موهفمان یگداــس دــیلوت اب </p>
@@ -14,10 +15,11 @@
             <button type="button">ثبت پروژه</button>
           </div>
         </div>
-        <div class="col-sm-6 d-flex flex-column justify-content-center align-items-end">
-          <h3>Vida Creative</h3>
-          <h1>Marketing Agency</h1>
-<!--          <img src="../assets/img/images/home_vector2.svg" alt="boat">-->
+        <div class="col-sm-6 d-flex flex-column justify-content-start align-items-end">
+          <div class="d-flex flex-column justify-content-center align-items-end">
+            <h3>Vida Creative</h3>
+            <h3>Marketing Agency</h3>
+          </div>
         </div>
       </div>
     </section>
