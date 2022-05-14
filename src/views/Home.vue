547f1cced8cef790d82d4070b198fb91b662bf0e
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-05-15 03:24:29 +0430

    Home Page Section 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,29 @@
 <template>
   <div id="home" data-safe>
     <section data-s-1 dir="rtl">
-      <div class="row">
-        <div></div>
-        <div class="col-sm-6 d-flex flex-column justify-content-start">
+      <div class="row position-relative">
+        <div class="position-absolute mouse">
+          <img src="../assets/img/icon/icon_mouse_black.svg" alt="mouse">
+        </div>
+        <div class="p1 col-sm-6 d-flex flex-column justify-content-start">
           <h1>آژانس خلاقیت ویدا</h1>
-          <p>تسا مزال هک نانچنآرطس و نوتس رد هلجم وچ تعنص زا موهفمان یگداــس دــیلوت اب </p>
+          <p class="mb-0">تسا مزال هک نانچنآرطس و نوتس رد هلجم وچ تعنص زا موهفمان یگداــس دــیلوت اب </p>
 
           <div class="d-flex justify-content-start align-items-center">
             <button type="button">ثبت پروژه</button>
             <button type="button">ثبت پروژه</button>
           </div>
         </div>
-        <div class="col-sm-6 d-flex flex-column justify-content-start align-items-end">
+        <div class="p2 col-sm-6 d-flex flex-column justify-content-start align-items-end">
           <div class="d-flex flex-column justify-content-center align-items-end">
             <h3>Vida Creative</h3>
             <h3>Marketing Agency</h3>
           </div>
+        </div>
+
+        <div class="bt_bar d-flex justify-content-center align-items-end">
+          <button type="button">ثبت پروژه</button>
+          <button type="button">ثبت پروژه</button>
         </div>
       </div>
     </section>
